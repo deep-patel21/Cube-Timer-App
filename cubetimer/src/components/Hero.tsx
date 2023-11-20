61bd546b263f3311746393cabea1892dcfd3b504
Rick_ <123428369+RickyRathod@users.noreply.github.com>
2023-11-19 20:06:30 -0500

    Edited Planning.txt

--- cubetimer/src/components/Hero.tsx
+++ cubetimer/src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='text-[100px] text-purple-600'>
+    <div className='text-[100px] text-green-600'>
         Hello
     </div>
   )
