b0e9b6df0409c6d085d361f57cb63d4e5362ec63
deep-patel21 <deepstudio21@gmail.com>
2023-11-27 21:30:00 -0500

    improvements to graph rendering by resizing to better fit div

--- cubetimer/src/components/ChartComponent.tsx
+++ cubetimer/src/components/ChartComponent.tsx
@@ -1,14 +1,79 @@
 'use client';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Chart from 'chart.js'
+import React, { useEffect } from 'react';
+import Chart from 'chart.js/auto';
 
-const ChartComponent = () => {
+interface ChartComponentProps {
+  solvesData: SolveDataPoint[];
+}
+
+const ChartComponent: React.FC<ChartComponentProps> = ({ solvesData }) => {
+  useEffect(() => {
+    const data = {
+      datasets: [
+        {
+          label: 'Cube Solves',
+          data: solvesData,
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 2,
+          pointBackgroundColor: 'rgba(75, 192, 192, 1)',
+          pointBorderColor: 'rgba(75, 192, 192, 1)',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false,
+        },
+      ],
+    };
+
+    const options = {
+      scales: {
+        x: {
+          type: 'linear',
+          position: 'bottom',
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of Cube Solves',
+          },
+        },
+        y: {
+          type: 'linear',
+          position: 'left',
+          scaleLabel: {
+            display: true,
+            labelString: 'Time (seconds)',
+          },
+        },
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              const dataPoint = context.dataset.data[context.dataIndex];
+              return `Solve ${context.dataIndex + 1}: ${dataPoint.y.toFixed(2)} seconds`;
+            },
+          },
+        },
+      },
+    };
+
+    const ctx = document.getElementById('solveChart');
+
+    const solveChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: options,
+    });
+
+    return () => {
+      solveChart.destroy();
+    };
+  }, [solvesData]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-800 text-white">
-        <canvas id="solveChart" width="400" height="200"></canvas>
+      <canvas id="solveChart" width="800" height="450"></canvas>
     </div>
   );
 };
+
 
 export default ChartComponent;
--- cubetimer/src/components/Hero.tsx
+++ cubetimer/src/components/Hero.tsx
@@ -17,19 +17,22 @@ const Hero = () => {
 
   return (
     <div className='flex flex-row w-screen h-screen'>
-      <div className='w-1/5'>
+      <div className='w-1/4'>
         <TimerLog recentTimes={recentTimers} />
       </div>
       <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-800 text-white">
         <div className='flex flex-row'>
-          <div>
-            <div className="text-[70px] font-bold mb-4 p-7 w-full justify-center text-center">Rubik's Cube Timer</div>
-            <div className="text-lg mb-8 text-center text-bold justify-center">
-              Developed by Deep Patel and Ricky Rathod
+          <div className='p-1 mt-0'>
+            <div className="text-[70px] font-bold mb-4 p-7 w-full h-[225px] justify-center text-center">
+              Rubik's Cube Timer
+              <div className="text-lg mb-8 text-center justify-center mt-[-10px]">
+                Developed by Deep Patel and Ricky Rathod
+              </div>
+              
             </div>
           </div>
           <div>
-            <video loop muted autoPlay width="400" height="225">
+            <video loop muted autoPlay width="300" height="169">
                 <source src="/Media/TheCuberAnimation.mp4" type="video/mp4" style={{ backgroundColor: 'transparent' }}/>
                 Your browser does not support the video tag.
               </video>
